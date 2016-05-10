84668c5579f5561d54d9f214c17c7b693e5ef680
Alex Savin <karismafilms@gmail.com>
2016-05-10 11:56:02 +0100

    Adding support for parsing filenames for datetime, and changed API to use options object

--- src/index.js
+++ src/index.js
@@ -9,9 +9,19 @@
 //
 // {
 //   "docs": [
-//       {"_id": "0", "integer": 0, "string": "0"},
-//       {"_id": "1", "integer": 1, "string": "1"},
-//       {"_id": "2", "integer": 2, "string": "2"}
+//     {
+//       "_id": "55d3a8b0-169d-11e6-aca2-f711d4732412",
+//       "filename": "2012-06-07-microsoft-bizspark-european-summit.html.md",
+//       "attributes": {
+//         "title": "Microsoft BizSpark European Summit"
+//       },
+//       "body": "",
+//       "html": "",
+//       "datetime": {
+//         "locale": "Thu Jun 07 2012 00:00:00 GMT+0100",
+//         "iso": "2012-06-06T23:00:00.000Z"
+//       }
+//     }
 //   ]
 // }
 
@@ -19,39 +29,52 @@ import * as mdparser from "markdown-parse";
 import * as fs from 'fs';
 import * as uuid from "node-uuid";
 
+// Using old school require because of https://github.com/moment/moment/issues/2608
+const moment = require('moment');
+
 let fileList = [];
 
-function readFiles(fileList, dirname) {
+function readFiles(fileList, dirname, parseDate) {
   let docList = [];
   fileList.forEach(function(filename) {
     const content = fs.readFileSync(dirname + '/' + filename, 'utf-8');
     mdparser.default(content, function(err, result) {
-      docList.push({
+      const newDoc = {
         _id: uuid.v1(),
         filename: filename,
         ...result
-      });
+      };
+
+      if(parseDate) {
+        const fileDate = moment(filename);
+        newDoc.datetime = {
+          locale: fileDate.toLocaleString(),
+          iso: fileDate.toISOString()
+        };
+      }
+
+      docList.push(newDoc);
     });
   });
   return {docs: docList};
 };
 
-function mdToCouchJson(dirname) {
+function mdToCouchJson(dirname, parseDate) {
   try {
     const fileList = fs.readdirSync(dirname);
     const mdList = fileList.filter(function(item) {
       return /\.md$/.test(item);
     });
-    return readFiles(mdList, dirname)
+    return readFiles(mdList, dirname, parseDate);
   } catch(e) {
     console.log(e);
   }
 }
 
-module.exports.default = function(dirname) {
-  if(!dirname) {
+module.exports.default = function(options) {
+  if(options === undefined || !options.hasOwnProperty('dirname')) {
     return new Error('Path is not provided');
   } else {
-    return mdToCouchJson(dirname);
+    return mdToCouchJson(options.dirname, options.parseDate);
   }
 };
--- test/mdToCouch.test.js
+++ test/mdToCouch.test.js
@@ -6,26 +6,36 @@ const assert = chai.assert;
 
 describe('mdToCouch', () => {
   it('contains correct structure compatible with CouchDB', () => {
-    expect(mdToCouch(__dirname).docs).to.exist;
-    expect(mdToCouch(__dirname).docs).to.be.an('array');
+    expect(mdToCouch({dirname: __dirname}).docs).to.exist;
+    expect(mdToCouch({dirname: __dirname}).docs).to.be.an('array');
   });
 
-  it('returns error when folder is not specified', () => {
+  it('returns error when options are not specified', () => {
     assert.isDefined(mdToCouch());
     assert.typeOf(mdToCouch(), 'Error');
   });
 
+  it('returns error when dirname option is not specified', () => {
+    assert.typeOf(mdToCouch({a: true}), 'Error');
+  });
+
   it('works with custom directory argument', () => {
-    assert.isDefined(mdToCouch(__dirname));
-    expect(mdToCouch(__dirname).docs.length).to.equal(3);
+    assert.isDefined(mdToCouch({dirname: __dirname}));
+    expect(mdToCouch({dirname: __dirname}).docs.length).to.equal(3);
   });
 
   it('puts body of the markdown file into document', () => {
-    expect(mdToCouch(__dirname).docs[0].body).to.exist;
+    expect(mdToCouch({dirname: __dirname}).docs[0].body).to.exist;
   });
 
   it('puts name of the markdown file into document', () => {
-    expect(mdToCouch(__dirname).docs[0].filename).to.exist;
-    expect(mdToCouch(__dirname).docs[0].filename).to.equal('2012-06-07-microsoft-bizspark-european-summit.html.md');
+    expect(mdToCouch({dirname: __dirname}).docs[0].filename).to.exist;
+    expect(mdToCouch({dirname: __dirname}).docs[0].filename).to.equal('2012-06-07-microsoft-bizspark-european-summit.html.md');
+  });
+
+  it('parses filename for date time when flag is specified', () => {
+    expect(mdToCouch({dirname: __dirname, parseDate: true}).docs[0].datetime).to.exist;
+    expect(mdToCouch({dirname: __dirname, parseDate: true}).docs[0].datetime.iso).to.exist;
+    expect(mdToCouch({dirname: __dirname, parseDate: true}).docs[0].datetime.locale).to.equal('Thu Jun 07 2012 00:00:00 GMT+0100');
   });
 });
